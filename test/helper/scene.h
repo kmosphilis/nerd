2927d47b409cd0e6141f93beaaa4682832710103
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-01-23 15:02:40 +0200

    Change helper functions to macros.

--- test/helper/scene.h
+++ test/helper/scene.h
@@ -1,10 +1,54 @@
+#include <check.h>
+
 #include "../../src/scene.h"
 
 #ifndef __SCENE_HELPER_H__
 #define __SCENE_HELPER_H__
 
-void ck_assert_scene_eq(const Scene * const scene1, const Scene * const scene2);
-void ck_assert_scene_notempty(const Scene * const scene);
-void ck_assert_scene_empty(const Scene * const scene);
+/**
+ * @brief Check two Scenes to determine if they are equal (scene1 == scene2).
+ *
+ * @param X The first Scene to compare.
+ * @param Y The second Scene to compare.
+ */
+#define ck_assert_scene_eq(X, Y) do { \
+    const Scene * const s1 = (X); \
+    const Scene * const s2 = (Y); \
+    if (!s1) { \
+        ck_assert_ptr_null(s1); \
+    } else if (!s2) { \
+        ck_assert_ptr_null(s2); \
+    } else { \
+        unsigned int i; \
+        ck_assert_int_eq(s1->size, s2->size); \
+        for (i = 0; i < s1->size; ++i) { \
+            ck_assert_literal_eq(&(s1->observations[i]), &(s2->observations[i])); \
+        } \
+    } \
+} while (0)
+
+#define _ck_assert_scene_empty(X, OP) do { \
+    const Scene * const s = (X); \
+    if (!s) { \
+        ck_assert_ptr_null(s); \
+    } else { \
+        _ck_assert_int(s->size, OP, 0); \
+        _ck_assert_ptr_null(s->observations, OP); \
+    } \
+} while (0)
+
+/**
+ * @brief Check if a Scene is empty.
+ *
+ * @param X The Scene to check.
+ */
+#define ck_assert_scene_empty(X) _ck_assert_scene_empty(X, ==)
+
+/**
+ * @brief Check if a Scene is not empty.
+ *
+ * @param X The Scene to check.
+ */
+#define ck_assert_scene_notempty(X) _ck_assert_scene_empty(X, !=)
 
 #endif
