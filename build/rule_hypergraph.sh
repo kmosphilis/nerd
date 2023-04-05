eaf09700bddd9ceb8b60df2b50d4d230e6e6cb42
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-04-05 14:53:41 +0300

    Add compiler warnings and required files.

--- build/rule_hypergraph.sh
+++ build/rule_hypergraph.sh
@@ -3,6 +3,7 @@ set -x
 cd "${0%/*}"
 mkdir -p ../bin
 rm -f ../bin/rule_hypergraph
-gcc -g -std=c2x -o ../bin/rule_hypergraph ../src/literal.c ../src/scene.c ../src/context.c \
-../src/rule.c ../src/rule_hypergraph.c ../libs/prb.o -lm -lcheck
+gcc -g -std=c2x -Wall -Wextra -o ../bin/rule_hypergraph ../src/literal.c ../src/scene.c \
+../src/context.c ../src/rule.c ../src/int_vector.c ../src/rule_queue.c ../src/knowledge_base.c \
+../src/rule_hypergraph.c ../libs/prb.o -lm -lcheck
 ../bin/rule_hypergraph
