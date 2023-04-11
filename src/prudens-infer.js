4271df95442f966f5bb060e3d72aa0d4f901827d
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-04-11 15:05:26 +0300

    Update prudens-js connection script.
    Update the script to also find and return the inferences of the Rules
    defeated by the observation.

--- src/prudens-infer.js
+++ src/prudens-infer.js
@@ -5,23 +5,41 @@ const fs = require('fs');
 fs.readFile(".temp", (err, data) => {
     if (!err) {
         const passed_data = data.toString().split("\n");
-        let test = JSON.parse(passed_data[0]);
-        test["constraints"] = new Map();
-        const result = prudens.forwardChaining(test, JSON.parse(passed_data[1])["context"]);
+        let kB = JSON.parse(passed_data[0]);
+        kB["constraints"] = new Map();
+        const result = prudens.forwardChaining(kB, JSON.parse(passed_data[1])["context"]);
 
-        const graphKeys = Object.keys(result.graph);
+        const inferred = new Set();
 
-        let literals = [];
-
-        for (let key in result.graph) {
+        for (const key in result.graph) {
             result.graph[key] = result.graph[key].filter((element) => !element.name.startsWith('$'));
             if (result.graph[key].length > 0) {
-                literals.push(`${key}`);
+                inferred.add(`${key}`);
             }
         }
-        fs.writeFile(".temp", literals.join(" "), (err) => {
+
+        for (const item of result.defeatedRules) {
+            if (item["by"]["name"].startsWith('$')) {
+                const literal_sign = item["defeated"]["head"]
+                const literal = parsers.literalToString(item["defeated"]["head"]);
+                let opposed_literal;
+                if (literal.startsWith('-')) {
+                    opposed_literal = literal.slice(1);
+                } else {
+                    opposed_literal = "-" + literal;
+                }
+
+                if (!inferred.has(opposed_literal)) {
+                    inferred.add(parsers.literalToString(item["defeated"]["head"]));
+                }
+            }
+        }
+
+        fs.writeFile(".temp", Array.from(inferred.values()).join(" "), (err) => {
             if (err) throw err;
             return;
         });
+    } else {
+        console.log("File not found.");
     }
 });
