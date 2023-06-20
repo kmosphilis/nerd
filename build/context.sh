56881473550444b068218e7cfaa39521ee526f6d
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-06-20 13:54:03 +0300

    Remove a redundancy.

--- build/context.sh
+++ build/context.sh
@@ -3,7 +3,6 @@ executable="../bin/context"
 set -x
 cd "${0%/*}"
 mkdir -p ../bin
-echo $executable
 rm -f $executable
 gcc -g -std=c2x -Wall -Wextra -o $executable ../src/nerd_utils.c ../src/literal.c ../src/scene.c\
  ../src/context.c ../test/context.c -lcheck -L../libs/pcg-c-0.94/src -lpcg_random\
