5b009b605f98e8d07ca7800b557f46992d36cd38
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-08-25 10:24:55 +0300

    Update the script to manually find the performance of the results.

--- plotting_scripts/create_results_plot.py
+++ plotting_scripts/create_results_plot.py
@@ -1,14 +1,83 @@
 import sys
+import re
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
-def create_plot(directory: Path):
+def calculate_correct_abstained(observation: list, filepath: Path, labels: list):
+    correct = 0; abstained = 0
+
+    with filepath.open() as file:
+        content = file.readlines()
+        if (len(observation) != len(content)):
+            exit(3)
+        for i, current_line in enumerate(content):
+            label_to_find = set(observation[i]).intersection(labels)
+            label_found = set(current_line.strip().split(" ")).intersection(labels)
+
+            if (label_to_find == label_found):
+                correct +=1
+            elif (not label_found):
+                abstained += 1
+
+    return correct / len(observation), abstained / len(observation)
+
+def create_plot(directory: Path, labels: Path):
     info_file = directory/"info"
-    train_results = directory/"train_results"
-    test_results = directory/"test_results"
+    results = directory/"results"
+    if (not results.exists() or not results.is_dir()):
+        exit(2)
+
+    training_file = results/"training-dataset.txt"
+    training_dataset = []
+    testing_file = results/"testing-dataset.txt"
+    testing_dataset = []
+
+    with training_file.open() as training:
+        for current_line in training:
+            training_dataset.append(current_line.strip().split(" "))
+
+    with testing_file.open() as testing:
+        for current_line in testing:
+            testing_dataset.append(current_line.strip().split(" "))
+
+    _labels = []
+    with labels.open() as y:
+        _labels = y.readline().strip().split(" ")
+
+    kb_result_directories = sorted(results.glob("iteration_*-instance_*"))
+
+    total = re.findall('\d+', kb_result_directories[-1].name)
+    iterations = int(total[0])
+    instances = int(total[1])
+    if (len(total) > iterations * instances):
+        instances = int(re.findall('\d+', kb_result_directories[-(instances + 1)].name)[1])
+
+    training_ratios = np.full((iterations * instances, 2), np.nan)
+    testing_ratios = np.full((iterations * instances, 2), np.nan)
+
+    for kb in kb_result_directories:
+        training_inferences = kb/"train.txt"
+        testing_inferences = kb/"test.txt"
+
+        indices = re.findall('\d+', kb.name)
+        iteration_index = int(indices[0]) - 1
+        instance_index = int(indices[1]) - 1
+        current_index = (iteration_index * instances) + instance_index
+
+        correct, abstained = calculate_correct_abstained(training_dataset, training_inferences,
+                                                         _labels)
+
+        training_ratios[current_index][0] = correct
+        training_ratios[current_index][1] = abstained
+
+        correct, abstained = calculate_correct_abstained(testing_dataset, testing_inferences,
+                                                         _labels)
+
+        testing_ratios[current_index][0] = correct
+        testing_ratios[current_index][1] = abstained
+
     fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(16, 9), layout='constrained')
 
     with info_file.open() as info:
@@ -26,58 +95,26 @@ def create_plot(directory: Path):
     fig.supylabel("Performance")
     fig.supxlabel("Iterations")
 
-    with train_results.open() as train:
-        results = []
-        lines = train.readlines()
-        for line in lines:
-            result = line.strip().split(' ')
-            results.append([float(n) for n in result])
+    x = np.linspace(1, instances * iterations, training_ratios.shape[0], True)
 
-        results.sort()
-        results = pd.DataFrame(results)
-        groups = results.groupby(0)
-
-        for i, group in enumerate(groups):
-            result = group[1].to_numpy()
-
-            x = np.linspace(result[0, 0] - 1, result[0, 0], result.shape[0])
-            axes[0].plot(x, result[:, 2], label='correct' if i == 0 else None, color='b')
-            axes[0].plot(x, result[:, 3], label='abstain' if i == 0 else None, color='g')
-            axes[0].plot(x, 1 - result[:, 2] - result[:, 3], label='incorrect' if i == 0 else None, color='r')
-
-        axes[0].set_xticks(range(1, groups.ngroups + 1))
-
+    axes[0].plot(x, training_ratios[:, 0], label='correct', color='b')
+    axes[0].plot(x, training_ratios[:, 1], label='abstain', color='g')
+    axes[0].plot(x, 1 - training_ratios[:, 0] - training_ratios[:, 1], label='incorrect', color='r')
     axes[0].set_ylabel("Training")
     axes[0].grid(axis='y', alpha=0.5)
     axes[0].grid(axis='x', color='k', alpha=1)
     axes[0].autoscale(True, axis= 'x', tight=True)
+    axes[0].set_xticks(range(1, iterations * instances, instances), range(1, iterations + 1))
     axes[0].legend()
 
-    with test_results.open() as test:
-        results = []
-        lines = test.readlines()
-        for line in lines:
-            result = line.strip().split(' ')
-            results.append([float(n) for n in result])
-
-        results.sort()
-        results = pd.DataFrame(results)
-        groups = results.groupby(0)
-
-        for i, group in enumerate(groups):
-            result = group[1].to_numpy()
-
-            x = np.linspace(result[0, 0] - 1, result[0, 0], result.shape[0])
-            axes[1].plot(x, result[:, 2], label='correct' if i == 0 else None, color='b')
-            axes[1].plot(x, result[:, 3], label='abstain' if i == 0 else None, color='g')
-            axes[1].plot(x, 1 - result[:, 2] - result[:, 3], label='incorrect' if i == 0 else None, color='r')
-
-        axes[1].set_xticks(range(1, groups.ngroups + 1))
-
+    axes[1].plot(x, testing_ratios[:, 0], label='correct', color='b')
+    axes[1].plot(x, testing_ratios[:, 1], label='abstain', color='g')
+    axes[1].plot(x, 1 - testing_ratios[:, 0] - testing_ratios[:, 1], label='incorrect', color='r')
     axes[1].set_ylabel("Testing")
     axes[1].grid(axis='y', alpha=0.5)
     axes[1].grid(axis='x', color='k', alpha=1)
     axes[1].autoscale(True, axis='x', tight=True)
+    axes[1].set_xticks(range(1, iterations * instances, instances), range(1, iterations + 1))
     axes[1].legend()
 
     plots_directory = directory/"plots"
@@ -85,13 +122,18 @@ def create_plot(directory: Path):
     fig.savefig(plots_directory/"performance_plot.pdf", bbox_inches='tight', dpi=150)
 
 def main():
-    if len(sys.argv) == 1:
-        print("Please include the path to the test.")
+    if len(sys.argv) != 3:
+        print("Please include the path to the test, and the path to the labels file.")
         return 1
     else:
         path = Path(sys.argv[1])
         if (path.exists() and path.is_dir()):
-            create_plot(path)
+            labels = None
+            if (len(sys.argv) == 3):
+                _labels = Path(sys.argv[2])
+                if (_labels.exists() and _labels.is_file()):
+                    labels = _labels
+            create_plot(path, labels)
         else:
             print(f"'{path}' Path does not exist.")
 
