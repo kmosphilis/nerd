544ef827d6d2cf317dab99f08ac608512ec0e0ac
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-11-16 09:24:10 +0200

    Change the script to behave as initially intended.
    It saves all the models created at each additional instance for future
    evaluation. Add an additional algorithm - decision tree classifier -
    and change fold-r++ to fold-rm.

---
+++ evaluation_helpers/online-SOTA.py
@@ -0,0 +1,149 @@
+import os
+import pickle
+import subprocess
+import sys
+from pathlib import Path
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+import tqdm
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.tree import DecisionTreeClassifier
+
+import foldrm as frm
+from xgboost import XGBClassifier
+
+CREATE_MODE = 0o740
+
+
+def accuracy_abstaintion_score(y: np.ndarray, y_hat: np.ndarray) -> Tuple[float, float]:
+    assert y.shape == y_hat.shape
+
+    abstained = (y_hat == None).sum()
+    return (y == y_hat).sum() / (y.shape[0] - abstained), abstained / y.shape[0]
+
+
+def main(
+    training_dataset_path: Path,
+    testing_dataset_path: Path,
+    label: str,
+    seed: int | None = None,
+):
+    training_data = pd.read_csv(training_dataset_path)
+    testing_data = pd.read_csv(testing_dataset_path)
+    assert label in training_data.columns and label in testing_data.columns
+
+    columns = np.ma.masked_equal(training_data.columns.values, label).compressed()
+
+    y_train = training_data[label].replace(np.nan, "").values
+    X_train = training_data.drop(columns=label).replace(np.nan, "").values
+
+    y_test = testing_data[label].replace(np.nan, "").values
+    X_test = testing_data.drop(columns=label).replace(np.nan, "").values
+
+    current_dir = Path(os.getcwd())
+
+    foldrm_dir = current_dir / "foldrm" / f"seed{seed}"
+    foldrm_dir.mkdir(CREATE_MODE, exist_ok=True)
+    frm_classifer = frm.Classifier(
+        attrs=list(columns) + [label], numeric=[], label=label
+    )
+
+    print("FOLD-RM")
+    for i in tqdm.tqdm(range(1, len(X_train) + 1)):
+        combined = np.append(
+            X_train[:i], np.reshape(y_train[:i], (y_train[:i].shape[0], 1)), axis=1
+        )
+
+        frm_classifer.fit(combined.tolist())
+
+        with (foldrm_dir / f"iteration_{i}.pickle").open("wb") as file:
+            pickle.dump(frm_classifer, file)
+
+    data_encoder = OrdinalEncoder()
+
+    label_encoding = {}
+    for i in range(y_train.shape[0]):
+        if not str(y_train[i]).isdigit() and not y_train[i] in label_encoding.keys():
+            current_label = y_train[i]
+            label_encoding[current_label] = len(label_encoding)
+            y_train[y_train == current_label] = label_encoding[current_label]
+            y_test[y_test == current_label] = label_encoding[current_label]
+
+    y_train = y_train.astype(int)
+    y_test = y_test.astype(int)
+    X_train = data_encoder.fit_transform(X_train)
+    X_test = data_encoder.transform(X_test)
+
+    decision_tree_dir = current_dir / "decision_tree" / f"seed{seed}"
+    decision_tree_dir.mkdir(CREATE_MODE, exist_ok=True)
+    decision_tree = DecisionTreeClassifier(random_state=seed)
+
+    print("\nDecision Tree")
+    for i in tqdm.tqdm(range(1, len(X_train) + 1)):
+        decision_tree.fit(X_train[:i], y_train[:i])
+
+        with (decision_tree_dir / f"iteration_{i}.pickle").open("wb") as file:
+            pickle.dump(decision_tree, file)
+
+    xgboost_dir = current_dir / "xgboost" / f"seed{seed}"
+    xgboost_dir.mkdir(CREATE_MODE, exist_ok=True)
+    xgboost_classifier = XGBClassifier(random_state=seed)
+
+    print("\nXGBoost")
+    for i in tqdm.tqdm(range(1, len(X_train) + 1)):
+        xgboost_classifier.fit(X_train[:i], y_train[:i])
+
+        file = xgboost_dir / f"iteration_{i}.json"
+        xgboost_classifier.save_model(file)
+
+    for file_type, dir in zip(
+        [".pickle", ".pickle", ".json"], [foldrm_dir, decision_tree_dir, xgboost_dir]
+    ):
+        if (
+            subprocess.run(
+                f"tar -c -I 'zstd -5' -f {dir}/models.tar.zst {dir}/*{file_type}",
+                shell=True,
+            ).returncode
+            == 0
+        ):
+            for f in dir.glob(f"*{file_type}"):
+                os.remove(f)
+
+
+if __name__ == "__main__":
+    if 5 < len(sys.argv) or len(sys.argv) < 4:
+        print(
+            "A training dataset, a testing dataset, and the name of its label are "
+            "required. Optionally a random seed can be given in the end."
+        )
+        exit(1)
+
+    seed = None
+    if len(sys.argv) == 5:
+        if sys.argv[4].isdigit():
+            seed = int(sys.argv[4])
+        else:
+            print(f"The given seed {seed} is not valid. Please provide a number")
+            exit(2)
+
+    training_dataset = Path(sys.argv[1])
+    testing_dataset = Path(sys.argv[2])
+
+    if training_dataset.exists():
+        if training_dataset.is_file():
+            if testing_dataset.exists():
+                if testing_dataset.is_file():
+                    main(training_dataset, testing_dataset, sys.argv[3], seed=seed)
+                    exit(0)
+                else:
+                    print(f"'{testing_dataset.name}' is not a file.")
+            else:
+                print(f"'{testing_dataset.name}' does not exist.")
+        else:
+            print(f"'{training_dataset.name}' is not a file.")
+    else:
+        print(f"'{training_dataset.name}' does not exist.")
+
+    exit(2)
