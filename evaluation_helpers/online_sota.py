06819674aedac3e0e835c23ea0a6e3c0dc924336
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2023-11-22 14:33:31 +0200

    Add better path naming, and save additional information.

--- evaluation_helpers/online_sota.py
+++ evaluation_helpers/online_sota.py
@@ -6,13 +6,12 @@ import time
 from pathlib import Path
 from typing import Tuple
 
+import foldrm as frm
 import numpy as np
 import pandas as pd
 import tqdm
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.tree import DecisionTreeClassifier
-
-import foldrm as frm
 from xgboost import XGBClassifier
 
 CREATE_MODE = 0o740
@@ -38,8 +37,24 @@ def main(
 
     current_dir = Path(os.getcwd())
 
-    foldrm_dir = current_dir / "foldrm" / f"seed{seed}"
-    foldrm_dir.mkdir(CREATE_MODE, parents=True, exist_ok=True)
+    foldrm_dir = current_dir / "foldrm"
+    foldrm_dir.mkdir(CREATE_MODE, exist_ok=True)
+    foldrm_trial: Path = None
+
+    folder_created = False
+    while not folder_created:
+        try:
+            foldrm_trial = foldrm_dir / f"trial={len(list(foldrm_dir.glob('trial=*')))}"
+            foldrm_trial.mkdir(CREATE_MODE)
+            folder_created = True
+        except FileExistsError:
+            pass
+
+    with (foldrm_trial / "info").open("w") as info:
+        info.write(f"seed={seed}\n")
+        info.write(f"training_dataset={training_dataset_path.resolve()}\n")
+        info.write(f"testing_dataset={testing_dataset_path.resolve()}\n")
+
     frm_classifer = frm.Classifier(
         attrs=list(columns) + [label], numeric=[], label=label
     )
@@ -57,7 +72,7 @@ def main(
 
         total_time += ending_time - starting_time
 
-        with (foldrm_dir / f"iteration_{i}.pickle").open("wb") as file:
+        with (foldrm_trial / f"iteration_{i}.pickle").open("wb") as file:
             pickle.dump(frm_classifer, file)
     print(
         f"Online version took: {total_time} ms\nOffline version took: "
@@ -79,8 +94,27 @@ def main(
     X_train = data_encoder.fit_transform(X_train)
     X_test = data_encoder.transform(X_test)
 
-    decision_tree_dir = current_dir / "decision_tree" / f"seed{seed}"
-    decision_tree_dir.mkdir(CREATE_MODE, parents=True, exist_ok=True)
+    decision_tree_dir = current_dir / "foldrm"
+    decision_tree_dir.mkdir(CREATE_MODE, exist_ok=True)
+    decision_tree_trial: Path = None
+
+    folder_created = False
+    while not folder_created:
+        try:
+            decision_tree_trial = (
+                decision_tree_dir
+                / f"trial={len(list(decision_tree_dir.glob('trial=*')))}"
+            )
+            decision_tree_trial.mkdir(CREATE_MODE)
+            folder_created = True
+        except FileExistsError:
+            pass
+
+    with (decision_tree_trial / "info").open("w") as info:
+        info.write(f"seed={seed}\n")
+        info.write(f"training_dataset={training_dataset_path.resolve()}\n")
+        info.write(f"testing_dataset={testing_dataset_path.resolve()}\n")
+
     decision_tree = DecisionTreeClassifier(random_state=seed)
     total_time = 0
 
@@ -92,15 +126,33 @@ def main(
 
         total_time += ending_time - starting_time
 
-        with (decision_tree_dir / f"iteration_{i}.pickle").open("wb") as file:
+        with (decision_tree_trial / f"iteration_{i}.pickle").open("wb") as file:
             pickle.dump(decision_tree, file)
     print(
         f"Online version took: {total_time} ms\nOffline version took: "
         f"{ending_time - starting_time} ms"
     )
 
-    xgboost_dir = current_dir / "xgboost" / f"seed{seed}"
-    xgboost_dir.mkdir(CREATE_MODE, parents=True, exist_ok=True)
+    xgboost_dir = current_dir / "xgboost"
+    xgboost_dir.mkdir(CREATE_MODE, exist_ok=True)
+    xgboost_trial: Path = None
+
+    folder_created = False
+    while not folder_created:
+        try:
+            xgboost_trial = (
+                xgboost_dir / f"trial={len(list(xgboost_dir.glob('trial=*')))}"
+            )
+            xgboost_trial.mkdir(CREATE_MODE)
+            folder_created = True
+        except FileExistsError:
+            pass
+
+    with (xgboost_trial / "info").open("w") as info:
+        info.write(f"seed={seed}\n")
+        info.write(f"training_dataset={training_dataset_path.resolve()}\n")
+        info.write(f"testing_dataset={testing_dataset_path.resolve()}\n")
+
     xgboost_classifier = XGBClassifier(random_state=seed)
     total_time = 0
 
@@ -112,7 +164,7 @@ def main(
 
         total_time += ending_time - starting_time
 
-        file = xgboost_dir / f"iteration_{i}.json"
+        file = xgboost_trial / f"iteration_{i}.json"
         xgboost_classifier.save_model(file)
 
     print(
@@ -121,7 +173,8 @@ def main(
     )
 
     for file_type, dir in zip(
-        [".pickle", ".pickle", ".json"], [foldrm_dir, decision_tree_dir, xgboost_dir]
+        [".pickle", ".pickle", ".json"],
+        [foldrm_trial, decision_tree_trial, xgboost_trial],
     ):
         if (
             subprocess.run(
