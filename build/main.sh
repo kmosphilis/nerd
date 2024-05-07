42bf4f57b50ac250ab44476e3bc06b0d38be2415
Kyriacos Mosphilis <k.mosphilis@cyens.org.cy>
2024-05-07 15:56:11 +0300

    Change the dependencies of the build scripts.

--- build/main.sh
+++ build/main.sh
@@ -5,7 +5,7 @@ cd "${0%/*}"
 mkdir -p ../bin
 rm -f $executable
 gcc -O3 -std=gnu2x -Wall -Wextra -o $executable ../src/nerd_utils.c ../src/literal.c ../src/scene.c\
- ../src/context.c ../src/rule.c ../src/rule_queue.c ../src/rule_hypergraph.c ../libs/prb.o\
+ ../src/context.c ../src/rule.c ../src/rule_queue.c ../src/queue.c ../src/rule_hypergraph.c ../libs/prb.o\
  ../src/knowledge_base.c ../src/sensor.c ../src/nerd_helper.c ../src/metrics.c ../src/nerd.c\
  ../src/main.c -lm -L../libs/pcg-c-0.94/src -lpcg_random -I../libs/pcg-c-0.94/include/\
  -I../libs/avl-2.0.3/
